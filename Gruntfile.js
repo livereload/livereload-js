e660cf57d11a14b5391767aedfc93d358a491ea8
smhg <sam.hauglustaine@gmail.com>
2018-12-07 21:29:10 +0100

    remove coffeescript build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,11 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    coffee: {
-      src: {
-        expand: true,
-        cwd: 'src',
-        src: '*.coffee',
-        dest: 'lib',
-        ext: '.js'
-      }
-    },
-
     browserify: {
       dist: {
         options: {
           transform: ['envify']
         },
-        src: ['lib/startup.js'],
+        src: ['src/startup.js'],
         dest: 'dist/livereload.js'
       },
 
@@ -36,12 +25,10 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-mocha-test');
 
-  grunt.registerTask('build', ['coffee', 'browserify:dist']);
+  grunt.registerTask('build', ['browserify:dist']);
   grunt.registerTask('test', ['mochaTest']);
   grunt.registerTask('default', ['build', 'test']);
-
 };
