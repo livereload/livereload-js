93e5e3739d55e6a0ba10d9007a0ded9861022249
Lukas Ehnle <lukas@owlabout.de>
2019-09-01 15:49:19 +0200

    add tests for fallback port and recognizing port 80

--- test/options_test.js
+++ test/options_test.js
@@ -55,6 +55,20 @@ describe('Options', function () {
     return assert.strictEqual(9876, options.port);
   });
 
+  it('should fallback to port 35729', function () {
+    const dom = new JSDOM('<script src="http://somewhere.com/132324324/23243443/4343/livereload.js"></script>');
+    const options = Options.extract(dom.window.document);
+    assert.ok(options);
+    return assert.strictEqual(35729, options.port);
+  });
+
+  it('should recognize port 80', function () {
+    const dom = new JSDOM('<script src="http://somewhere.com:80/132324324/23243443/4343/livereload.js"></script>');
+    const options = Options.extract(dom.window.document);
+    assert.ok(options);
+    return assert.strictEqual(80, options.port);
+  });
+
   return it('should set https when using an https URL ', function () {
     const dom = new JSDOM('<script src="https://somewhere.com:9876/livereload.js"></script>');
 
