bc675302f614cd88fc812da2a288c30b07c2339a
ivan baktsheev <dot.and.thing@gmail.com>
2019-09-23 16:34:25 +0300

    [test] silenced live css messages

--- test/reloader_test.js
+++ test/reloader_test.js
@@ -134,7 +134,7 @@ describe('Reloader', () => {
             getElementsByTagName () { return []; }
           }
         },
-        console,
+        { log () {} },
         Timer
       );
 
@@ -231,7 +231,7 @@ describe('Reloader', () => {
             getElementsByTagName () { return []; }
           }
         },
-        console,
+        { log () {} },
         Timer
       );
 
